7e159c5400fdca496c719997b04d80160e7f06ad
zhzhao <zhzhao999@126.com>
2019-09-11 23:17:21 +0800

    menu

---
+++ src/api/system.js
@@ -0,0 +1,37 @@
+import request from '@/utils/request'
+
+// 获取子级菜单
+export function getMenuList(id) {
+  return request({
+    url: '/sys/menu/one',
+    method: 'get',
+    params: id
+  })
+}
+
+// 新建菜单
+export function menuSava(data) {
+  return request({
+    url: '/sys/menu/save',
+    method: 'post',
+    data
+  })
+}
+
+// 编辑更新菜单
+export function menuUpload(data) {
+  return request({
+    url: '/sys/menu/update',
+    method: 'post',
+    data
+  })
+}
+
+// 删除菜单
+export function menuDelete(id) {
+  return request({
+    url: '/sys/menu/delete',
+    method: 'post',
+    id
+  })
+}
--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -12,78 +12,79 @@
           @node-click="handleNodeClick"
           ref="tree1"></el-tree>
       </el-col>
-      <!-- <el-col :span="18">
-				<div style="margin-bottom: 10px;">
-					<el-row :gutter="10">
-						<el-col :span="10">
-							<el-input placeholder="请输入菜单名称" v-model="listQuery.name" size='small' clearable>
-								<template slot="prepend">菜单</template>
-							</el-input>
-						</el-col>
-						<el-col :span="7">
-							<el-button @click="perSearch" type="primary" size="small">查询
-							</el-button>
-						</el-col>
-						<el-col :span="7">
-							<div style="float: right;">
-								<el-button style="margin: 0;" size="small" @click="handleCreate">添加</el-button>
-							</div>
-							<div style="clear: both;"></div>
-						</el-col>
-					</el-row>
-				</div>
-				<el-table id="mr-table" :key='tableKey' size="mini" align="center" :data="list.content" border stripe fit highlight-current-row @selection-change="setSelection" style="width: 100%">
-					<el-table-column type="selection" width="55" label="全选" align="center"></el-table-column>
-					<el-table-column type="index" label="序号" width="65" align="center">
-						<template scope="scope">
-							<span>{{scope.row.SORT}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column align="center" :show-overflow-tooltip="true" label="权限名称">
-						<template scope="scope">
-							<span>{{scope.row.NAME}}</span>
-						</template>
-					</el-table-column>
-
-					<el-table-column label="页面名称" :show-overflow-tooltip="true" align="center" min-width="200">
-						<template scope="scope">
-							<span>{{scope.row.PAGE_URL}}</span>
-						</template>
-					</el-table-column>
-
-					<el-table-column align="center" label="图标">
-						<template scope="scope">
-							<span>{{scope.row.MENU_ICON}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column align="center" :show-overflow-tooltip="true" label="备注">
-						<template scope="scope">
-							<span>{{scope.row.REMARK}}</span>
-						</template>
-					</el-table-column>
-					<el-table-column align="center" label="操作">
-						<template scope="scope">
-							<p id="p_icon">
-								<i class="el-icon-view" title="查看" @click="handleDetail(scope.row)"></i>
-								<i class="el-icon-edit" title="编辑" @click="handleUpdate(scope.row)"></i>
-								<i class="el-icon-delete" title="删除" @click="deleteCurrent(scope.row)"></i>
-							</p>
-						</template>
-					</el-table-column>
-
-				</el-table>
-			</el-col> -->
+      <el-col :span="18">
+        <el-button @click="handleAdd('operForm')">新增</el-button>
+        <el-table ref="multipleTable" :data="tableData" :loading="loading" border tooltip-effect="dark">
+          <el-table-column
+            type="index">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="菜单名称">
+          </el-table-column>
+          <el-table-column
+            prop="pageUrl"
+            label="页面路径">
+          </el-table-column>
+          <el-table-column
+            prop="icon"
+            label="图标">
+          </el-table-column>
+          <el-table-column
+            prop="remark"
+            label="备注">
+          </el-table-column>
+          <el-table-column
+            label="操作">
+            <template slot-scope="scope">
+              <el-button type="text" @click="handleEdit(scope.row)">编辑</el-button>
+              <el-button type="text" @click="handleDelete(scope.row.id)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
     </el-row>
+
+    <!-- 新增、编辑 -->
+    <el-dialog :title="operTitle" :visible.sync="operVisible">
+      <el-form ref="operForm" :model="operData" label-width="100px">
+        <el-form-item label="上级菜单" prop="parentId">
+          <el-input v-model="operData.parentId"></el-input>
+        </el-form-item>
+        <el-form-item label="菜单名称" prop="name">
+          <el-input v-model="operData.name"></el-input>
+        </el-form-item>
+        <el-form-item label="页面路径" prop="pageUrl">
+          <el-input v-model="operData.pageUrl"></el-input>
+        </el-form-item>
+        <el-form-item label="序号" prop="sort">
+          <el-input v-model="operData.sort"></el-input>
+        </el-form-item>
+        <el-form-item label="图标" prop="icon">
+          <el-input v-model="operData.icon"></el-input>
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="operData.remark"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="operVisible = false">取 消</el-button>
+        <el-button type="primary" v-if="operType == 'add'" @click="operSave()">确 定</el-button>
+        <el-button type="primary" v-if="operType == 'edit'" @click="operUpload()">确 定</el-button>
+      </span>
+    </el-dialog>
 
   </div>
 </template>
 
 <script>
 import { getMenu } from '@/api/login'
+import { getMenuList, menuSava, menuUpload, menuDelete } from '@/api/system'
 export default {
   name: 'menu-manager',
   data() {
     return {
+      // 菜单树
       filterText: '',
       menuTree: [],
       defaultProps: {
@@ -91,12 +92,25 @@ export default {
         label: 'name',
         id: 'id'
       },
-      listQuery: {
-        page: 1,
-        rows: 20,
-        permissionId: '4028c6396bff97a2016bff9900000000',
-        name: ''
-      },
+
+      // 子级菜单
+      loading: false,
+      permissionId: '4028c6396bff97a2016bff9900000000',
+      tableData: [],
+
+      // 新增、编辑
+      operVisible: false,
+      operType: 'add',
+      operTitle: '新增菜单',
+      operData: {
+        parentId: '', // 父id
+        name: '', // 菜单名称
+        pageUrl: '', // 访问路径
+        icon: '', // icon
+        sort: '', // 排序
+        remark: '', // 备注
+      }
+
     }
   },
   created() {
@@ -111,17 +125,69 @@ export default {
       })
     },
     getList() {
-      // getMenuList(this.listQuery).then(res => {
-
-      // })
+      this.loading = true
+      getMenuList({id: this.permissionId}).then(res => {
+        this.loading = false
+        this.tableData = res.datas
+      })
     },
     filterNode(value, data) {
       if(!value) return true
       return data.name.indexOf(value) !== -1
     },
     handleNodeClick(data) {
-      // this.listQuery.permissionId = data.id
+      this.permissionId = data.id
       this.getList();
+    },
+
+    // 新增菜单
+    handleAdd(formName) {
+      this.operVisible = true
+      this.operType = 'add'
+      this.operTitle = '新增菜单'
+      this.$refs[formName].resetFields();
+    },
+    operSave() {
+      menuSava(this.operData).then(res => {
+        this.$message({
+          message: res.repMsg,
+          type: 'success'
+        })
+        this.operVisible = false
+        this.getMenuTree()
+        this.getList()
+      })
+    },
+    // 编辑菜单
+    handleEdit(row) {
+      this.operVisible = true
+      this.operType = 'edit'
+      this.operTitle = '编辑菜单'
+      this.operData = row
+    },
+    operUpload() {
+      menuUpload(this.operData).then(res => {
+        this.$message({
+          message: res.repMsg,
+          type: 'success'
+        })
+        this.operVisible = false
+        this.getMenuTree()
+        this.getList()
+      })
+    },
+    // 删除菜单
+    handleDelete(id) {
+      this.$confirm('确定删除？').then(() => {
+        menuDelete({id: id}).then(res => {
+          this.$message({
+            message: res.repMsg,
+            type: 'success'
+          })
+          this.getMenuTree()
+          this.getList()
+        })
+      })
     },
   },
   watch: {
@@ -131,3 +197,9 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+  .menu-tree {
+    margin-right: 20px;
+  }
+</style>
